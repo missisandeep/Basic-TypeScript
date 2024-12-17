75b04218ac3a3d4c706e996bea5d995e8249a303
sandeep <kietsandeepkumar@gmail.com>
2024-12-17 11:29:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+function x(a, b) {
+    return a + b;
+}
+console.warn('sandeep - ', x(2, 3));
---
+++ app.ts
@@ -0,0 +1,5 @@
+function x(a:number, b:number){
+    return a+b;
+}
+
+console.warn('sandeep - ', x(2,3))
